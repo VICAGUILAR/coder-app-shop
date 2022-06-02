174f9f2d21b7d40fcee696b2000c271df0899b33
VICAGUILAR <isc_vhpa@hotmail.com>
2022-06-01 20:46:08 -0500

    desafios pendientes

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,18 +1,26 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 export const ItemDetail = ({data}) => {
+
+    const onAdd = (quantity) => {
+        alert(`Agregaste al carrito ${quantity} `);
+    }
+
     return (
         <div className="container">
             <div className="detail">
-                <img className="detail_image" src="{data.image}" alt="controladora"/>
+                <img className="detail_image" src={data.image} alt="controladora"/>
                 <div className="content">
                     <h1>{data.title}</h1>
+                    <ItemCount initial={1} stock={5} onAdd={onAdd}/>
 
                 </div>
 
             </div>
 
         </div>
+        
     );
 }
 
